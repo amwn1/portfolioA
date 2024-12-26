9730ffa097e945eb458b155d948617376446b11e
amwn1 <amanmalik3105@gmail.com>
2024-12-26 11:17:23 +0530

    changes 1

--- src/components/aboutme.tsx
+++ src/components/aboutme.tsx
@@ -1,16 +1,20 @@
 import React from "react";
-import profileImage from "../resources/images/profile2.png"; // Replace with your image path
+import profileImage from "../resources/images/profile21.png"; // Replace with your image path
 
 const AboutMe: React.FC = () => {
   return (
     <div className="min-h-screen bg-[#000B58] flex items-center justify-center">
-      <div className="flex items-center justify-center w-full max-w-4xl">
+      {/* Image */}
+      <div className="flex justify-center items-center">
         <img
           src={profileImage}
           alt="Profile"
-          className="w-1/3 h-auto object-cover"
+          className="w-[400px] h-auto object-cover z-20"
         />
-        <div className="bg-[#98DED9] p-8 ml-[-50px] shadow-lg max-w-lg">
+      </div>
+      {/* Content Box */}
+      <div className="flex justify-center items-center p-8">
+        <div className="bg-[#98DED9] p-8 shadow-lg max-w-lg z-10 relative">
           <h1 className="text-3xl font-bold mb-4">ABOUT ME</h1>
           <p className="text-lg">
             I am a dedicated software developer with a knack for creating efficient and scalable solutions. My expertise spans both front-end and back-end technologies, allowing me to deliver comprehensive and user-friendly applications. I thrive in collaborative environments, leveraging agile methodologies to enhance team productivity and drive innovation. My commitment to continuous learning ensures I stay ahead of industry trends, bringing the latest insights to every project. With a focus on quality and performance, I am passionate about building software that not only meets but exceeds expectations. Let's connect to explore how I can contribute to your next groundbreaking project.
