2f85355f20faba6c8dd659186bea4b9f9c7387dc
amwn1 <amanmalik3105@gmail.com>
2024-12-27 17:30:31 +0530

    making the image responsive

--- src/components/aboutme.tsx
+++ src/components/aboutme.tsx
@@ -9,30 +9,38 @@ const AboutMe: React.FC = () => {
       {/* Container for Image and Content */}
       <div className="flex items-center justify-center relative max-w-6xl mx-auto">
         {/* Image */}
-        <div className="relative">
+        <div className="relative hidden md:block">
           <img
             src={profileImage}
             alt="Profile"
-            className="w-[300px] md:w-[400px] h-auto object-cover z-20 relative"
-            style={{ transform: 'translateX(-30px)' }}
+            className="w-[300px] md:w-[400px] lg:w-[500px] h-auto object-cover z-20 relative"
+            style={{ transform: 'translateX(25px)' }}
           />
         </div>
+        
         {/* Content Box */}
-        <div className="bg-[#98DED9] p-8 md:p-12 shadow-lg max-w-lg z-10 relative ml-[-50px] md:ml-[-80px]">
-          <h1 className="text-3xl font-kalnia mb-10">ABOUT ME</h1>
-          <p className="text-md font-joan">
-            I am a dedicated software developer with a knack for creating efficient and scalable solutions. My expertise spans both front-end and back-end technologies, allowing me to deliver comprehensive and user-friendly applications. I thrive in collaborative environments, leveraging agile methodologies to enhance team productivity and drive innovation. My commitment to continuous learning ensures I stay ahead of industry trends, bringing the latest insights to every project. With a focus on quality and performance, I am passionate about building software that not only meets but exceeds expectations. Let's connect to explore how I can contribute to your next groundbreaking project.
-          </p>
-          <div className="flex space-x-4 mt-4">
-            <a href="https://www.linkedin.com" className="text-white">
-              <FontAwesomeIcon icon={faLinkedin} size="2x" />
-            </a>
-            <a href="https://www.instagram.com" className="text-white">
-              <FontAwesomeIcon icon={faInstagram} size="2x" />
-            </a>
-            <a href="https://www.github.com" className="text-white">
-              <FontAwesomeIcon icon={faGithub} size="2x" />
-            </a>
+        <div className="bg-[#98DED9] p-8 md:p-12 shadow-lg w-full max-w-3xl z-10 relative ml-[-50px] md:ml-[-80px] flex">
+          {/* Example Text */}
+          <div className="flex-none mr-4 md:mr-20">
+            <p></p>
+          </div>
+          {/* About Me Content */}
+          <div className="mr-4">
+            <h1 className="text-3xl font-kalnia mb-10">ABOUT ME</h1>
+            <p className="text-md font-joan">
+              I am a dedicated software developer with a knack for creating efficient and scalable solutions. My expertise spans both front-end and back-end technologies, allowing me to deliver comprehensive and user-friendly applications. I thrive in collaborative environments, leveraging agile methodologies to enhance team productivity and drive innovation. My commitment to continuous learning ensures I stay ahead of industry trends, bringing the latest insights to every project. With a focus on quality and performance, I am passionate about building software that not only meets but exceeds expectations. Let's connect to explore how I can contribute to your next groundbreaking project.
+            </p>
+            <div className="flex space-x-4 mt-4">
+              <a href="https://www.linkedin.com" className="text-white">
+                <FontAwesomeIcon icon={faLinkedin} size="2x" />
+              </a>
+              <a href="https://www.instagram.com" className="text-white">
+                <FontAwesomeIcon icon={faInstagram} size="2x" />
+              </a>
+              <a href="https://www.github.com" className="text-white">
+                <FontAwesomeIcon icon={faGithub} size="2x" />
+              </a>
+            </div>
           </div>
         </div>
       </div>
